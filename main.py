42d0f5c2ffbbf7f248b32f2995efe21764615052
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-08-23 23:41:14 -0600

    agregamos unos try para capturar los errores en los ingresos de los menus

--- main.py
+++ main.py
@@ -4,141 +4,165 @@ menu = Menus()
 opcion = 0
 a = False
 while a == False:
-    menu.menu_principal()
-    opcion = int(input("Ingrese una opcion: "))
-    match opcion:
-        case 1:
-            s = False
-            while s == False:
-                menu.menu_categorias()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 2:
-            s = False
-            while s == False:
-                menu.menu_productos()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 3:
-            s = False
-            while s == False:
-                menu.menu_clientes()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 4:
-            s = False
-            while s == False:
-                menu.menu_proveedores()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 5:
-            s = False
-            while s == False:
-                menu.menu_empleados()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 6:
-            s = False
-            while s == False:
-                menu.menu_compras()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        pass
-                    case 6:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 7:
-            s = False
-            while s == False:
-                menu.menu_ventas()
-                opcion = int(input("Ingrese una opcion: "))
-                match opcion:
-                    case 1:
-                        pass
-                    case 2:
-                        pass
-                    case 3:
-                        pass
-                    case 4:
-                        pass
-                    case 5:
-                        pass
-                    case 6:
-                        s = True
-                    case _:
-                        print("Opcion invalida")
-        case 8:
-            print("Gracias por usar el sistema")
-            a = True
-        case _:
-            print("Opcion invalida")
+    try:
+        menu.menu_principal()
+        opcion = int(input("Ingrese una opcion: "))
+        match opcion:
+            case 1:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_categorias()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 2:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_productos()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 3:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_clientes()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 4:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_proveedores()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 5:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_empleados()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 6:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_compras()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                pass
+                            case 6:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 7:
+                s = False
+                while s == False:
+                    try:
+                        menu.menu_ventas()
+                        opcion = int(input("Ingrese una opcion: "))
+                        match opcion:
+                            case 1:
+                                pass
+                            case 2:
+                                pass
+                            case 3:
+                                pass
+                            case 4:
+                                pass
+                            case 5:
+                                pass
+                            case 6:
+                                s = True
+                            case _:
+                                print("Opcion invalida")
+                    except Exception as ex:
+                        print(f"Ha ocurrido un error: {ex}")
+            case 8:
+                print("Gracias por usar el sistema")
+                a = True
+            case _:
+                print("Opcion invalida")
+    except Exception as ex:
+        print(f"Ha ocurrido un error: {ex}")
